72109dd5866e979c96f276b170720ef13fa8ee85
Josh Nichols <jnichols20091@gmail.com>
2023-06-23 02:34:07 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# from selenium import webdriver
+#
+#
+# class Browser:
+#     def init(self):
+#         self.driver = driver
+# browser = Browser(webdriver=webdriver.Chrome())
+# browser.init()
+# import os
+# import sys
+# import json
+# import robot
+#
+#
+# info = {
+#     "python_executable": sys.executable,
+#     "python_version": tuple(sys.version_info),
+#     "robot_version": robot.version,
+#     "enviroment": dict(os.environ),
+# }
+#
+# os = robot.count_files_in_directory("C:\\Users\\User\\PycharmProjects\\robotframework\\robotframework\\test")
+#
+# print(info.values())
+# print(os)
---
+++ test.robot
@@ -0,0 +1,14 @@
+*** Settings ***
+Library    String
+| Library | OperatingSystem
+Resource    keywords.resource
+
+*** Test Cases ***
+#Generate random string
+#    ${random_string}=    String.Generate Random String    10    [LETTERS]
+#    Log    ${random_string}
+
+
+Get File Size
+    ${file_size}=    OperatingSystem.Get File Size    ${CURDIR}${/}keywords.resource
+    Log    ${file_size}
